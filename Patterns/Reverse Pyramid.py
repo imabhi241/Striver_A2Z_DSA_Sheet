433fb615087c56390247295d48008b49e806cf24
Abhishek Kumar <imabhishek241@gmail.com>
2022-08-29 13:49:48 +0530

    Pyramid pattern

---
+++ Patterns/Reverse Pyramid.py
@@ -0,0 +1,11 @@
+l=1
+r=9
+for i in range(1,6):
+    for j in range(1,10):
+        if l<=j<=r:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    l+=1
+    r-=1        
+    print()            
